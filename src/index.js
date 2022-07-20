b808ec7798da257ada2d2015885a183b5c423e0b
mushfeque.zihan@gmail.com <mushfeque.zihan@gmail.com>
2022-07-20 14:28:29 -0400

    updated a word in line

--- src/index.js
+++ src/index.js
@@ -11,8 +11,9 @@ let city = {
 
 ReactDOM.render(
    <h1 id="heading" className="cool-text">
-       {city.name} is in {city.country}
+       {city.name} is in {city.country} - Europe
        </h1>,
+       
     document.getElementById('root'));
 
 
